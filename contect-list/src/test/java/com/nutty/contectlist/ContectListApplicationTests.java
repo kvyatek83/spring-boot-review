71765e6556d8b2d49fb38efaeedaade8acf5b3ac
nutty <kvyatek83@gmail.com>
2021-08-10 16:49:18 +0300

    Init Spring Boot SandBox

---
+++ contect-list/src/test/java/com/nutty/contectlist/ContectListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutty.contectlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContectListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
