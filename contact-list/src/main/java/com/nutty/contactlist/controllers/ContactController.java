94522a7b6548feab02a4c80f7dad781de767d943
nutty <kvyatek83@gmail.com>
2021-08-16 13:32:27 +0300

    log4j2

--- contact-list/src/main/java/com/nutty/contactlist/controllers/ContactController.java
+++ contact-list/src/main/java/com/nutty/contactlist/controllers/ContactController.java
@@ -3,7 +3,9 @@ package com.nutty.contactlist.controllers;
 import com.nutty.schema.api.ContactApi;
 import com.nutty.schema.models.Contact;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +16,16 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@Slf4j
 @NoArgsConstructor
 public class ContactController implements ContactApi {
     HashMap<String, Contact> contactList = new HashMap<>();
+    private static final Logger logger = LogManager.getLogger(ContactController.class);
 
     @Override
     public ResponseEntity<Void> createContact(@Valid Contact contact) {
-        log.info("createContact");
+        logger();
         if (contactList.containsKey(contact.getId())) {
-            log.error(String.format("Contact with id: %s already exist", contact.getId()));
+            logger.error(String.format("Contact with id: %s already exist", contact.getId()));
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
@@ -31,12 +33,20 @@ public class ContactController implements ContactApi {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    private void logger() {
+        logger.info("info createContact");
+        logger.error("error createContact");
+        logger.debug("debug createContact");
+        logger.warn("warn createContact");
+        logger.fatal("fatal createContact");
+    }
+
     @Override
     public ResponseEntity<Contact> getContact(String contactId) {
-        log.info("getContact");
+        logger();
 
         if (!contactList.containsKey(contactId)) {
-            log.error(String.format("Contact with id: %s is not exist", contactId));
+            logger.error(String.format("Contact with id: %s is not exist", contactId));
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(contactList.get(contactId), HttpStatus.OK);
@@ -44,20 +54,20 @@ public class ContactController implements ContactApi {
 
     @Override
     public ResponseEntity<List<Contact>> getContactList() {
-        log.info("getContactList");
+        logger();
         return new ResponseEntity<>(new ArrayList<>(contactList.values()), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> removeContact(String contactId) {
-        log.info("removeContact");
+        logger();
         contactList.remove(contactId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> updateContact(String contactId, @Valid Contact contact) {
-        log.info("updateContact");
+        logger();
         contactList.replace(contactId, contact);
         return new ResponseEntity<>(HttpStatus.OK);
     }
